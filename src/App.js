e8b62e68c426d1871e405e827421d4c8737f30d0
Arif Hussain <arif3337179@gmail.com>
2023-11-13 22:40:42 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -15,21 +15,6 @@ function App() {
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="#home">Arif Hussain</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll" className="justify-content-end">
-
-            <Nav style={style} className="mr-auto my-2 my-lg-0" navbarScroll>
-              <Nav.Link href="#action1" active>Home</Nav.Link>
-              <Nav.Link href="#action2">About Us</Nav.Link>
-              <NavDropdown title="Projects" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">P1</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">P2</NavDropdown.Item>
-                <NavDropdown.Item href="#action5">P3</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#">Contact </Nav.Link>
-            </Nav>
-
-          </Navbar.Collapse>
         </Container>
       </Navbar>
 
